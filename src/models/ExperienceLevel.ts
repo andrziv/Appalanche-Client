08bf60ff3eec4c6e116a2a8fc9b4657e89c6d6eb
andrziv <soveryhappyface@gmail.com>
2025-12-03 14:14:40 -0500

    Added new experience levels to match what the backend has

--- src/models/ExperienceLevel.ts
+++ src/models/ExperienceLevel.ts
@@ -1,9 +1,12 @@
 export class ExperienceLevel {
+    static readonly Intern = new ExperienceLevel('Intern', 'Not graduated yet');
     static readonly Entry = new ExperienceLevel('Entry Level', '0 years experience');
     static readonly Junior = new ExperienceLevel('Junior', '1–2 years experience');
     static readonly Intermediate = new ExperienceLevel('Intermediate', '3–5 years experience');
     static readonly Senior = new ExperienceLevel('Senior', '5–8 years experience');
-    static readonly StaffPlus = new ExperienceLevel('Staff+', '8+ years experience');
+    static readonly Staff = new ExperienceLevel('Staff', '>8 years experience');
+    static readonly Principal = new ExperienceLevel('Principal', '>10 years experience');
+    static readonly Manager = new ExperienceLevel('Manager', 'Managerial role');
 
     public readonly label: string;
     public readonly description: string;
@@ -15,12 +18,19 @@ export class ExperienceLevel {
 
     static values(): ExperienceLevel[] {
         return [
+            ExperienceLevel.Intern,
             ExperienceLevel.Entry,
             ExperienceLevel.Junior,
             ExperienceLevel.Intermediate,
             ExperienceLevel.Senior,
-            ExperienceLevel.StaffPlus,
+            ExperienceLevel.Staff,
+            ExperienceLevel.Principal,
+            ExperienceLevel.Manager,
         ];
+    }
+
+    get code(): string {
+        return label.toUpperCase().replace(` `, `_`);
     }
 
     toString() {
